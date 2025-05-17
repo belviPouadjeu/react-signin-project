37c5aa30a8f4148ceef41e4af81d7882b711ffe0
belviPouadjeu <pbelvinard21@gmail.com>
2025-05-17 13:06:58 +0200

    Setting up project with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="font-inter text-4xl font-bold text-gray-500 underline">
+        Tailwind fonctionne
+      </h1>
     </div>
-  );
+    )
 }
 
-export default App;
+export default App
